8a588591def6028d863e620033b85a4e136068f5
Đoàn Ngọc Minh Châu ୨ৎ <167704587+23520168@users.noreply.github.com>
2024-10-09 22:54:27 +0700

    Test

---
+++ Week5/ClientForm/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace ClientForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.rtbMessages = new System.Windows.Forms.RichTextBox();
+            this.btnSendMessage = new System.Windows.Forms.Button();
+            this.btnSendFile = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtServerIp = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtFriend = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // rtbMessages
+            // 
+            this.rtbMessages.Location = new System.Drawing.Point(34, 124);
+            this.rtbMessages.Name = "rtbMessages";
+            this.rtbMessages.Size = new System.Drawing.Size(1907, 820);
+            this.rtbMessages.TabIndex = 0;
+            this.rtbMessages.Text = "";
+            // 
+            // btnSendMessage
+            // 
+            this.btnSendMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSendMessage.Location = new System.Drawing.Point(34, 963);
+            this.btnSendMessage.Name = "btnSendMessage";
+            this.btnSendMessage.Size = new System.Drawing.Size(224, 118);
+            this.btnSendMessage.TabIndex = 1;
+            this.btnSendMessage.Text = "Send Message";
+            this.btnSendMessage.UseVisualStyleBackColor = true;
+            this.btnSendMessage.Click += new System.EventHandler(this.btnSendMessage_Click);
+            // 
+            // btnSendFile
+            // 
+            this.btnSendFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSendFile.Location = new System.Drawing.Point(34, 1109);
+            this.btnSendFile.Name = "btnSendFile";
+            this.btnSendFile.Size = new System.Drawing.Size(224, 81);
+            this.btnSendFile.TabIndex = 2;
+            this.btnSendFile.Text = "Send File";
+            this.btnSendFile.UseVisualStyleBackColor = true;
+            this.btnSendFile.Click += new System.EventHandler(this.btnSendFile_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(289, 1109);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(1652, 81);
+            this.progressBar.TabIndex = 3;
+            // 
+            // tb1
+            // 
+            this.tb1.Location = new System.Drawing.Point(289, 963);
+            this.tb1.Multiline = true;
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(1652, 118);
+            this.tb1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(222, 32);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Server Address";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(658, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 32);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(1121, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 32);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Friend";
+            // 
+            // txtServerIp
+            // 
+            this.txtServerIp.Location = new System.Drawing.Point(256, 40);
+            this.txtServerIp.Name = "txtServerIp";
+            this.txtServerIp.Size = new System.Drawing.Size(376, 38);
+            this.txtServerIp.TabIndex = 8;
+            this.txtServerIp.Text = "127.0.0.1";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(816, 43);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(226, 38);
+            this.txtUsername.TabIndex = 9;
+            this.txtUsername.Text = "Alice";
+            // 
+            // txtFriend
+            // 
+            this.txtFriend.AllowDrop = true;
+            this.txtFriend.Location = new System.Drawing.Point(1228, 43);
+            this.txtFriend.Name = "txtFriend";
+            this.txtFriend.Size = new System.Drawing.Size(230, 38);
+            this.txtFriend.TabIndex = 10;
+            this.txtFriend.Text = "Bob";
+            this.txtFriend.UseWaitCursor = true;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.BackColor = System.Drawing.Color.LightCyan;
+            this.btnConnect.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnect.Location = new System.Drawing.Point(1588, 8);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(331, 110);
+            this.btnConnect.TabIndex = 11;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = false;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(1997, 1234);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtFriend);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.txtServerIp);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb1);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnSendFile);
+            this.Controls.Add(this.btnSendMessage);
+            this.Controls.Add(this.rtbMessages);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbMessages;
+        private System.Windows.Forms.Button btnSendMessage;
+        private System.Windows.Forms.Button btnSendFile;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtServerIp;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtFriend;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Week5/ServerForm/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ServerForm
+{
+    public partial class Form1 : Form
+    {
+        private Socket _serverSocket;
+        private Socket _clientSocket;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Khi nhấn nút "Listen", bắt đầu lắng nghe kết nối
+        private void btnListen_Click(object sender, EventArgs e)
+        {
+            StartServer();
+        }
+
+        private void StartServer()
+        {
+            try
+            {
+                IPAddress ipAddress = IPAddress.Parse(txtIpAddrress.Text); // Lấy địa chỉ IP từ TextBox
+                _serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                _serverSocket.Bind(new IPEndPoint(ipAddress, 5000)); // Lắng nghe tại cổng 5000
+                _serverSocket.Listen(1);
+
+                AppendMessage("Server đang lắng nghe tại " + txtIpAddrress.Text);
+                _serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi khởi động server: " + ex.Message);
+            }
+        }
+
+        private void AcceptCallback(IAsyncResult ar)
+        {
+            _clientSocket = _serverSocket.EndAccept(ar);
+            AppendMessage("Client đã kết nối!");
+
+            byte[] buffer = new byte[1024];
+            _clientSocket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), buffer);
+        }
+
+        private void ReceiveCallback(IAsyncResult ar)
+        {
+            byte[] buffer = (byte[])ar.AsyncState;
+            int receivedBytes = _clientSocket.EndReceive(ar);
+            string receivedMessage = Encoding.UTF8.GetString(buffer, 0, receivedBytes);
+
+            if (receivedMessage.StartsWith("text:"))
+            {
+                // Xử lý tin nhắn văn bản
+                string messageContent = receivedMessage.Substring(5); // Bỏ qua "text:"
+                AppendMessage(messageContent); // Hiển thị tin nhắn trên Server
+            }
+            else if (receivedMessage.StartsWith("file:"))
+            {
+                // Xử lý file
+                string[] parts = receivedMessage.Substring(5).Split(':');
+                if (parts.Length == 2)
+                {
+                    string fileName = parts[0]; // Tên file
+                    string fileData = parts[1]; // Dữ liệu file
+                    SaveFile(fileName, fileData); // Lưu file
+                    AppendMessage("File đã nhận: " + fileName); // Hiển thị thông báo file đã nhận
+                }
+            }
+
+            // Tiếp tục lắng nghe tin nhắn tiếp theo
+            byte[] newBuffer = new byte[1024];
+            _clientSocket.BeginReceive(newBuffer, 0, newBuffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), newBuffer);
+        }
+
+        private void SaveFile(string fileName, string fileBase64)
+        {
+            string directoryPath = "ReceivedFiles";
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath); // Tạo thư mục nếu không tồn tại
+            }
+
+            byte[] fileBytes = Convert.FromBase64String(fileBase64);
+            File.WriteAllBytes(Path.Combine(directoryPath, fileName), fileBytes); // Lưu file vào thư mục ReceivedFiles
+        }
+
+
+        private void AppendMessage(string message)
+        {
+            Invoke((Action)delegate
+            {
+                rtbMessages.AppendText(message + Environment.NewLine);
+                Console.WriteLine(message); // Ghi log vào Console để kiểm tra
+            });
+        }
+
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            if (listBoxFiles.SelectedItem != null)
+            {
+                string fileName = listBoxFiles.SelectedItem.ToString();
+                string sourcePath = Path.Combine("ReceivedFiles", fileName);
+
+                if (File.Exists(sourcePath))
+                {
+                    // Tải file về (mở file để tải về hoặc thực hiện hành động khác)
+                    System.Diagnostics.Process.Start(sourcePath); // Mở file với ứng dụng mặc định
+                }
+                else
+                {
+                    MessageBox.Show("File không tồn tại.");
+                }
+            }
+        }
+    }
+}
