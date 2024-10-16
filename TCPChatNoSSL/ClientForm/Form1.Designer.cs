dc033da068e7821d458024302572f94d3a15a489
Đoàn Ngọc Minh Châu ୨ৎ <167704587+23520168@users.noreply.github.com>
2024-10-17 02:44:29 +0700

    Make color

--- TCPChatNoSSL/ClientForm/Form1.Designer.cs
+++ TCPChatNoSSL/ClientForm/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
             this.tb3 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
@@ -47,51 +48,56 @@
             // checkedListBox1
             // 
             this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Location = new System.Drawing.Point(548, 346);
-            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkedListBox1.Location = new System.Drawing.Point(1096, 670);
+            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(62, 38);
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 39);
             this.checkedListBox1.TabIndex = 0;
             // 
             // tb3
             // 
-            this.tb3.Location = new System.Drawing.Point(510, 12);
+            this.tb3.Location = new System.Drawing.Point(1020, 23);
+            this.tb3.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.tb3.Name = "tb3";
-            this.tb3.Size = new System.Drawing.Size(100, 22);
+            this.tb3.Size = new System.Drawing.Size(196, 38);
             this.tb3.TabIndex = 26;
             this.tb3.Text = "Bob";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(456, 12);
+            this.label4.Location = new System.Drawing.Point(912, 23);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(45, 16);
+            this.label4.Size = new System.Drawing.Size(95, 32);
             this.label4.TabIndex = 25;
             this.label4.Text = "Friend";
             // 
             // tb1
             // 
-            this.tb1.Location = new System.Drawing.Point(334, 13);
+            this.tb1.Location = new System.Drawing.Point(668, 25);
+            this.tb1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.tb1.Name = "tb1";
-            this.tb1.Size = new System.Drawing.Size(100, 22);
+            this.tb1.Size = new System.Drawing.Size(196, 38);
             this.tb1.TabIndex = 24;
             this.tb1.Text = "Alice";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(256, 15);
+            this.label3.Location = new System.Drawing.Point(512, 29);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(70, 16);
+            this.label3.Size = new System.Drawing.Size(144, 32);
             this.label3.TabIndex = 23;
             this.label3.Text = "Username";
             // 
             // btnConnect
             // 
-            this.btnConnect.Location = new System.Drawing.Point(628, 8);
+            this.btnConnect.Location = new System.Drawing.Point(1256, 16);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btnConnect.Name = "btnConnect";
-            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.Size = new System.Drawing.Size(150, 45);
             this.btnConnect.TabIndex = 22;
             this.btnConnect.Text = "Connect";
             this.btnConnect.UseVisualStyleBackColor = true;
@@ -99,34 +105,38 @@
             // 
             // tb2
             // 
-            this.tb2.Location = new System.Drawing.Point(123, 15);
+            this.tb2.Location = new System.Drawing.Point(246, 29);
+            this.tb2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.tb2.Name = "tb2";
-            this.tb2.Size = new System.Drawing.Size(100, 22);
+            this.tb2.Size = new System.Drawing.Size(196, 38);
             this.tb2.TabIndex = 21;
             this.tb2.Text = "127.0.0.1";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(14, 18);
+            this.label2.Location = new System.Drawing.Point(28, 35);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(100, 16);
+            this.label2.Size = new System.Drawing.Size(205, 32);
             this.label2.TabIndex = 20;
             this.label2.Text = "Server address";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(90, 56);
+            this.label1.Location = new System.Drawing.Point(180, 108);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.Size = new System.Drawing.Size(0, 32);
             this.label1.TabIndex = 19;
             // 
             // btnSendMessage
             // 
-            this.btnSendMessage.Location = new System.Drawing.Point(8, 326);
+            this.btnSendMessage.Location = new System.Drawing.Point(16, 632);
+            this.btnSendMessage.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btnSendMessage.Name = "btnSendMessage";
-            this.btnSendMessage.Size = new System.Drawing.Size(117, 94);
+            this.btnSendMessage.Size = new System.Drawing.Size(234, 182);
             this.btnSendMessage.TabIndex = 18;
             this.btnSendMessage.Text = "Send Message";
             this.btnSendMessage.UseVisualStyleBackColor = true;
@@ -134,33 +144,36 @@
             // 
             // sendMsgTextBox
             // 
-            this.sendMsgTextBox.Location = new System.Drawing.Point(130, 326);
+            this.sendMsgTextBox.Location = new System.Drawing.Point(260, 632);
+            this.sendMsgTextBox.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.sendMsgTextBox.Name = "sendMsgTextBox";
-            this.sendMsgTextBox.Size = new System.Drawing.Size(644, 96);
+            this.sendMsgTextBox.Size = new System.Drawing.Size(1284, 182);
             this.sendMsgTextBox.TabIndex = 17;
             this.sendMsgTextBox.Text = "";
             // 
             // rtb1
             // 
-            this.rtb1.Location = new System.Drawing.Point(8, 46);
+            this.rtb1.Location = new System.Drawing.Point(16, 89);
+            this.rtb1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.rtb1.Name = "rtb1";
-            this.rtb1.Size = new System.Drawing.Size(767, 269);
+            this.rtb1.Size = new System.Drawing.Size(1530, 517);
             this.rtb1.TabIndex = 16;
             this.rtb1.Text = "";
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(130, 435);
+            this.progressBar1.Location = new System.Drawing.Point(260, 843);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(642, 23);
+            this.progressBar1.Size = new System.Drawing.Size(1284, 45);
             this.progressBar1.TabIndex = 27;
             // 
             // btnSendFile
             // 
-            this.btnSendFile.Location = new System.Drawing.Point(8, 435);
-            this.btnSendFile.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSendFile.Location = new System.Drawing.Point(16, 843);
+            this.btnSendFile.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSendFile.Name = "btnSendFile";
-            this.btnSendFile.Size = new System.Drawing.Size(117, 23);
+            this.btnSendFile.Size = new System.Drawing.Size(234, 45);
             this.btnSendFile.TabIndex = 28;
             this.btnSendFile.Text = "Send File";
             this.btnSendFile.UseVisualStyleBackColor = true;
@@ -168,9 +181,10 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(780, 473);
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(1560, 916);
             this.Controls.Add(this.btnSendFile);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.tb3);
@@ -185,7 +199,8 @@
             this.Controls.Add(this.sendMsgTextBox);
             this.Controls.Add(this.rtb1);
             this.Controls.Add(this.checkedListBox1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Client Chat";
             this.ResumeLayout(false);
--- TCPChatNoSSL/ServerForm/Form1.Designer.cs
+++ TCPChatNoSSL/ServerForm/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.tb1 = new System.Windows.Forms.TextBox();
             this.rtb1 = new System.Windows.Forms.RichTextBox();
@@ -37,37 +38,37 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(42, 13);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(84, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(19, 16);
+            this.label1.Size = new System.Drawing.Size(40, 32);
             this.label1.TabIndex = 0;
             this.label1.Text = "IP";
             // 
             // tb1
             // 
-            this.tb1.Location = new System.Drawing.Point(72, 11);
-            this.tb1.Margin = new System.Windows.Forms.Padding(2);
+            this.tb1.Location = new System.Drawing.Point(144, 21);
+            this.tb1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tb1.Name = "tb1";
-            this.tb1.Size = new System.Drawing.Size(270, 22);
+            this.tb1.Size = new System.Drawing.Size(536, 38);
             this.tb1.TabIndex = 1;
             this.tb1.Text = "127.0.0.1";
             // 
             // rtb1
             // 
-            this.rtb1.Location = new System.Drawing.Point(17, 44);
-            this.rtb1.Margin = new System.Windows.Forms.Padding(2);
+            this.rtb1.Location = new System.Drawing.Point(34, 85);
+            this.rtb1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rtb1.Name = "rtb1";
-            this.rtb1.Size = new System.Drawing.Size(465, 294);
+            this.rtb1.Size = new System.Drawing.Size(926, 566);
             this.rtb1.TabIndex = 2;
             this.rtb1.Text = "";
             // 
             // btnListen
             // 
-            this.btnListen.Location = new System.Drawing.Point(350, 3);
-            this.btnListen.Margin = new System.Windows.Forms.Padding(2);
+            this.btnListen.Location = new System.Drawing.Point(700, 6);
+            this.btnListen.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnListen.Name = "btnListen";
-            this.btnListen.Size = new System.Drawing.Size(102, 28);
+            this.btnListen.Size = new System.Drawing.Size(204, 54);
             this.btnListen.TabIndex = 3;
             this.btnListen.Text = "Listen";
             this.btnListen.UseVisualStyleBackColor = true;
@@ -75,14 +76,16 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(497, 351);
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(994, 680);
             this.Controls.Add(this.btnListen);
             this.Controls.Add(this.rtb1);
             this.Controls.Add(this.tb1);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Server";
             this.ResumeLayout(false);
