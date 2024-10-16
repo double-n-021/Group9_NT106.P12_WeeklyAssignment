95166dc3029561639162ab0fbdec93ef03d08184
Le Nhat Trinh Nguyen <161190441+ovapil@users.noreply.github.com>
2024-10-17 02:29:10 +0700

    Add TCP Chat No SSL

---
+++ TCPChatNoSSL/ClientForm/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace ClientForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.tb3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.tb2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSendMessage = new System.Windows.Forms.Button();
+            this.sendMsgTextBox = new System.Windows.Forms.RichTextBox();
+            this.rtb1 = new System.Windows.Forms.RichTextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnSendFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(548, 346);
+            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(62, 38);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // tb3
+            // 
+            this.tb3.Location = new System.Drawing.Point(510, 12);
+            this.tb3.Name = "tb3";
+            this.tb3.Size = new System.Drawing.Size(100, 22);
+            this.tb3.TabIndex = 26;
+            this.tb3.Text = "Bob";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(456, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 16);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Friend";
+            // 
+            // tb1
+            // 
+            this.tb1.Location = new System.Drawing.Point(334, 13);
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(100, 22);
+            this.tb1.TabIndex = 24;
+            this.tb1.Text = "Alice";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(256, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 16);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Username";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(628, 8);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 22;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // tb2
+            // 
+            this.tb2.Location = new System.Drawing.Point(123, 15);
+            this.tb2.Name = "tb2";
+            this.tb2.Size = new System.Drawing.Size(100, 22);
+            this.tb2.TabIndex = 21;
+            this.tb2.Text = "127.0.0.1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 16);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Server address";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(90, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 19;
+            // 
+            // btnSendMessage
+            // 
+            this.btnSendMessage.Location = new System.Drawing.Point(8, 326);
+            this.btnSendMessage.Name = "btnSendMessage";
+            this.btnSendMessage.Size = new System.Drawing.Size(117, 94);
+            this.btnSendMessage.TabIndex = 18;
+            this.btnSendMessage.Text = "Send Message";
+            this.btnSendMessage.UseVisualStyleBackColor = true;
+            this.btnSendMessage.Click += new System.EventHandler(this.btnSendMessage_Click);
+            // 
+            // sendMsgTextBox
+            // 
+            this.sendMsgTextBox.Location = new System.Drawing.Point(130, 326);
+            this.sendMsgTextBox.Name = "sendMsgTextBox";
+            this.sendMsgTextBox.Size = new System.Drawing.Size(644, 96);
+            this.sendMsgTextBox.TabIndex = 17;
+            this.sendMsgTextBox.Text = "";
+            // 
+            // rtb1
+            // 
+            this.rtb1.Location = new System.Drawing.Point(8, 46);
+            this.rtb1.Name = "rtb1";
+            this.rtb1.Size = new System.Drawing.Size(767, 269);
+            this.rtb1.TabIndex = 16;
+            this.rtb1.Text = "";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(130, 435);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(642, 23);
+            this.progressBar1.TabIndex = 27;
+            // 
+            // btnSendFile
+            // 
+            this.btnSendFile.Location = new System.Drawing.Point(8, 435);
+            this.btnSendFile.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSendFile.Name = "btnSendFile";
+            this.btnSendFile.Size = new System.Drawing.Size(117, 23);
+            this.btnSendFile.TabIndex = 28;
+            this.btnSendFile.Text = "Send File";
+            this.btnSendFile.UseVisualStyleBackColor = true;
+            this.btnSendFile.Click += new System.EventHandler(this.btnSendFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 473);
+            this.Controls.Add(this.btnSendFile);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.tb3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tb1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.tb2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSendMessage);
+            this.Controls.Add(this.sendMsgTextBox);
+            this.Controls.Add(this.rtb1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Client Chat";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.TextBox tb3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox tb2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSendMessage;
+        private System.Windows.Forms.RichTextBox sendMsgTextBox;
+        private System.Windows.Forms.RichTextBox rtb1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnSendFile;
+    }
+}
+
---
+++ TCPChatNoSSL/ClientForm/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ClientForm
+{
+    public partial class Form1 : Form
+    {
+        private TcpClient tcpClient;
+        private StreamReader sReader;
+        private StreamWriter sWriter;
+        private Thread clientThread;
+        private int serverPort = 8000;
+        private bool stopTcpClient = true;
+        public const int BufferSize = 4096;
+        public const int FileBufferSize = 3072;
+        private string SaveFileName = string.Empty;
+        private MemoryStream fileSaveMemoryStream;
+        public enum
+            MessageType
+        {
+            Text,
+            FileEof,
+            FilePart,
+        }
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        [STAThread]
+        private void ClientRecv()
+        {
+            NetworkStream networkStream = tcpClient.GetStream();
+            byte[] buffer = new byte[BufferSize];
+            MemoryStream currentFileStream = null;
+            string currentFileName = null;
+
+            try
+            {
+                while (!stopTcpClient && tcpClient.Connected)
+                {
+                    int bytesRead = networkStream.Read(buffer, 0, buffer.Length);
+                    if (bytesRead == 0) continue;
+
+                    string headerAndMessage = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    string[] arrPayload = headerAndMessage.Split(';');
+
+                    if (arrPayload.Length >= 3)
+                    {
+                        string senderUsername = arrPayload[0];
+                        MessageType msgType = (MessageType)Enum.Parse(typeof(MessageType), arrPayload[1], true);
+                        string content = arrPayload[2];
+
+                        if (msgType == MessageType.Text)
+                        {
+                            UpdateChatHistoryThreadSafe($"{senderUsername}: {content}\n");
+                        }
+                        else if (msgType == MessageType.FilePart)
+                        {
+                            if (currentFileStream == null)
+                            {
+                                Invoke((Action)(() =>
+                                {
+                                    if (MessageBox.Show("Nhận file mới từ " + senderUsername + "?",
+                                        "Nhận file", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                                    {
+                                        SaveFileDialog sfd = new SaveFileDialog();
+                                        sfd.Filter = "All files (*.*)|*.*";
+                                        if (sfd.ShowDialog() == DialogResult.OK)
+                                        {
+                                            currentFileName = sfd.FileName;
+                                            currentFileStream = new MemoryStream();
+                                        }
+                                    }
+                                }));
+                            }
+
+                            if (currentFileStream != null)
+                            {
+                                byte[] fileData = Convert.FromBase64String(content);
+                                currentFileStream.Write(fileData, 0, fileData.Length);
+                            }
+                        }
+                        else if (msgType == MessageType.FileEof)
+                        {
+                            if (currentFileStream != null && currentFileName != null)
+                            {
+                                if (content.Length > 0)
+                                {
+                                    byte[] finalData = Convert.FromBase64String(content);
+                                    currentFileStream.Write(finalData, 0, finalData.Length);
+                                }
+
+                                currentFileStream.Position = 0;
+                                using (FileStream fs = File.Create(currentFileName))
+                                {
+                                    currentFileStream.CopyTo(fs);
+                                }
+
+                                UpdateChatHistoryThreadSafe($"File đã được lưu tại: {currentFileName}\n");
+
+                                currentFileStream.Dispose();
+                                currentFileStream = null;
+                                currentFileName = null;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Lỗi nhận dữ liệu: {ex.Message}");
+            }
+        }
+        private delegate void SafeCallDelegate(string text);
+
+        private delegate void SaveFileConfirmDialogDelegate(DialogResult result);
+
+        private void UpdateChatHistoryThreadSafe(string text)
+        {
+            if (rtb1.InvokeRequired)
+            {
+                var d = new SafeCallDelegate(UpdateChatHistoryThreadSafe);
+                rtb1.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                rtb1.Text += text;
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                stopTcpClient = false;
+                this.tcpClient = new TcpClient();
+                this.tcpClient.Connect(new IPEndPoint(IPAddress.Parse(tb2.Text), serverPort));
+                this.sWriter = new StreamWriter(tcpClient.GetStream())
+                {
+                    AutoFlush = true
+                };
+
+                sWriter.WriteLine(this.tb1.Text);
+
+                clientThread = new Thread(this.ClientRecv);
+                clientThread.Start();
+
+                MessageBox.Show("Connected to server");
+            }
+            catch (SocketException sockEx)
+            {
+                MessageBox.Show(sockEx.Message, "Network error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        private void btnSendFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (OpenFileDialog ofd = new OpenFileDialog())
+                {
+                    ofd.Filter = "All files (*.*)|*.*";
+                    if (ofd.ShowDialog() == DialogResult.OK)
+                    {
+                        NetworkStream ns = tcpClient.GetStream();
+                        string receiverUsername = tb3.Text;
+
+                        using (FileStream fs = File.OpenRead(ofd.FileName))
+                        {
+                            byte[] buffer = new byte[FileBufferSize];
+                            int bytesRead;
+
+                            while ((bytesRead = fs.Read(buffer, 0, buffer.Length)) > 0)
+                            {
+                                byte[] actualData = new byte[bytesRead];
+                                Array.Copy(buffer, actualData, bytesRead);
+
+                                string base64Data = Convert.ToBase64String(actualData);
+                                string message = $"{receiverUsername};{MessageType.FilePart};{base64Data}";
+
+                                byte[] sendBuffer = Encoding.UTF8.GetBytes(message);
+                                ns.Write(sendBuffer, 0, sendBuffer.Length);
+                                ns.Flush();
+
+                                Thread.Sleep(10);
+                            }
+
+                            string eofMessage = $"{receiverUsername};{MessageType.FileEof};";
+                            byte[] eofBuffer = Encoding.UTF8.GetBytes(eofMessage);
+                            ns.Write(eofBuffer, 0, eofBuffer.Length);
+                            ns.Flush();
+                        }
+
+                        UpdateChatHistoryThreadSafe($"Đã gửi file: {Path.GetFileName(ofd.FileName)}\n");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Lỗi gửi file: {ex.Message}");
+            }
+        }
+        private void btnSendMessage_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                rtb1.Text +=  "You: " + sendMsgTextBox.Text + "\n";
+                NetworkStream ns = tcpClient.GetStream();
+                string allInOneMsg = $"{tb3.Text};{MessageType.Text.ToString()};{sendMsgTextBox.Text}";
+                byte[] sendingBytes = Encoding.UTF8.GetBytes(allInOneMsg);
+                ns.Write(sendingBytes, 0, sendingBytes.Length);
+                sendMsgTextBox.Text = "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
